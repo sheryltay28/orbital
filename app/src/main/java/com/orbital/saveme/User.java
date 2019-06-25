b44ef0fde94c4b4be0f3ebf1debcd668a5195c36
Sheryl <tsgirl28@gmail.com>
2019-06-25 22:39:55 +0800

    upload user info

---
+++ app/src/main/java/com/orbital/saveme/User.java
@@ -0,0 +1,11 @@
+package com.orbital.saveme;
+
+public class User {
+    public String userName;
+    public String userNumber;
+
+    public User(String userName, String userNumber) {
+        this.userName = userName;
+        this.userNumber = userNumber;
+    }
+}
